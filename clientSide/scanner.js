610d83b168ac09da59d047c7186ab49ba05fb8bc
Sven Haaijman <silvervoid@icloud.com>
2016-10-17 21:48:06 +0200

    nearly working

--- clientSide/scanner.js
+++ clientSide/scanner.js
@@ -1,5 +1,6 @@
 var mysql = require('mysql');
 var prompt = require('prompt');
+var readlineSync = require('readline-sync');
 
 var connection = mysql.createConnection({
     host: 'localhost',
@@ -9,27 +10,33 @@ var connection = mysql.createConnection({
     port: '8889'
 });
 
-
-prompt.start();
 connection.connect();
 
 function stuff() {
-    prompt.get(['userCode', 'productCode'], function(err, result) {
-        var productCodeString = result.productCode.replace('P', '');
+        var productCodeIn = readlineSync.question('Product code: ');
+        var productCodeString = productCodeIn.replace('P', '');
         var productCode = parseInt(productCodeString);
-        var requestType = result.userCode.charAt(0);
+        var userCode = readlineSync.question('User code: ');
+        var requestType = userCode.charAt(0);
         var q;
         if (requestType == 'I') {
             q = "UPDATE `TheCodeSpace_inventory_overview` SET `In`=`In`+1, `Out`=`Out`-1 WHERE id=" + productCode.toString();
         } else if (requestType == 'U') {
             q = "UPDATE `TheCodeSpace_inventory_overview` SET `In`=`In`-1, `Out`=`Out`+1 WHERE id=" + productCode.toString();
         }
-        console.log(productCode, result.userCode, requestType);
+        console.log(productCode, userCode, requestType);
         connection.query(q, function(err, rows, fields) {
-            console.log(err);
+            console.log('done');
         });
-    });
-    connection.end();
-    prompt.stop();
-}
+        var again = readlineSync.question('again? [y/n]');
+        if (again == 'y') {
+          stuff();
+        }
+        else if (again == 'n') {
+          connection.end();
+        }
+        else {
+          console.log('error');
+        }
+    }
 stuff();
