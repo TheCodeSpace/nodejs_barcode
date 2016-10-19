740f34a2dd430bee72641b6803d9255b4d4e68d5
Sven Haaijman <silvervoid@icloud.com>
2016-10-19 12:45:05 +0200

    tried somethin

--- clientSide/scanner.js
+++ clientSide/scanner.js
@@ -21,22 +21,30 @@ function stuff() {
     var q;
     var adminMode = false;
     var done = false;
-    if (requestType == 'I') {
-        q = "UPDATE `TheCodeSpace_inventory_overview` SET `In`=`In`+1, `Out`=`Out`-1 WHERE id=" + productCode.toString();
+    var quantity = 1;
+
+    if (requestType == 'Q') {
+      quantity = parseInt(readlineSync.question('Quantity: ');
+    }
+    else if (requestType == 'I') {
+        q = "UPDATE `TheCodeSpace_inventory_overview` SET `In`=`In`+" + quantity + ", `Out`=`Out`-" + quantity + " WHERE id=" + productCode.toString();
     } else if (requestType == 'U') {
-        q = "UPDATE `TheCodeSpace_inventory_overview` SET `In`=`In`-1, `Out`=`Out`+1 WHERE id=" + productCode.toString();
+        q = "UPDATE `TheCodeSpace_inventory_overview` SET `In`=`In`-" + quantity + ", `Out`=`Out`+" + quantity + " WHERE id=" + productCode.toString();
     } else {
         //
-        //ADMIN MODe
+        //ADMIN MODE
         if (userCode == 'ADM1N') {
             console.log('\n==[ ADMIN MODE ACTIVATED ]==');
             adminMode = true;
             var userCode = readlineSync.question('User code: ');
             var requestType = userCode.charAt(0);
-            if (requestType == 'I') {
-                q = "UPDATE `TheCodeSpace_inventory_overview` SET `In`=`In`+1 WHERE id=" + productCode.toString();
+            if (requestType == 'Q') {
+              quantity = readlineSync.question('Quantity: ');
+            }
+            else if (requestType == 'I') {
+                q = "UPDATE `TheCodeSpace_inventory_overview` SET `In`=`In`+" + quantity + " WHERE id=" + productCode.toString();
             } else if (requestType == 'U') {
-                q = "UPDATE `TheCodeSpace_inventory_overview` SET `In`=`In`+1 WHERE id=" + productCode.toString();
+                q = "UPDATE `TheCodeSpace_inventory_overview` SET `In`=`In`+" + quantity + " WHERE id=" + productCode.toString();
             } else {
                 console.log('error');
                 connection.end();
@@ -47,7 +55,7 @@ function stuff() {
     console.log(date);
     console.log(productCode, userCode);
     var userCodeParsed = userCode.substring(1);
-    connection.query("INSERT INTO `TheCodeSpace_inventory_history` (`User`, `Product`, `Action`, `Admin`, `Date`) VALUES (" + userCodeParsed + ", " + productCode + ", '" + requestType + "', " + adminMode + ", '" + date + "')", function(err, rows, fields) {
+    connection.query("INSERT INTO `TheCodeSpace_inventory_history` (`User`, `Product`, `Quantity` `Action`, `Admin`, `Date`) VALUES (" + userCodeParsed + ", " + productCode + ", " + quantity + ", '" + requestType + "', " + adminMode + ", '" + date + "')", function(err, rows, fields) {
         if (err) {
             console.log(err);
         }
